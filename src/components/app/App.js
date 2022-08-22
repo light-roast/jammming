c2854a810c4ff5cde8c3702a97db491c51acdae8
Daniel Echeverri <echeverri121@gmail.com>
2022-08-22 09:55:32 -0500

    Step 25 completed

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import {SearchBar} from '../SearchBar/SearchBar';
+import {SearchResults} from '../SearchResults/SearchResults';
+
 
 function App() {
   return (
     <div>
   <h1>Ja<span className="highlight">mmm</span>ing</h1>
   <div className="App">
-    {/* <SearchBar /> */}
+    <SearchBar />
     <div className="App-playlist">
-    {/* <SearchResults /> */}
+    <SearchResults />
     {/* <Playlist/> */}
     </div>
   </div>
