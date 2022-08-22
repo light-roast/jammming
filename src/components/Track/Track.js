432449a21d097c7513796c4e4b6953154eacb8f2
Daniel Echeverri <echeverri121@gmail.com>
2022-08-22 14:13:57 -0500

    Step 28 completed - Track Component

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,17 +1,17 @@
 import { Component } from "react";
-
+import "./Track.css";
 export class Track extends Component.Class {
     renderAction() {
-        
+        return (this.props.isRemoval) ? <button className="Track-action">-</button> : <button className="Track-action">+</button>;
     }
     render(){
         return (
             <div className="Track">
                 <div className="Track-information">
-                <h3><!-- track name will go here --></h3>
-                <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                <h3>track name will go here</h3>
+                <p>artist will go here | track album will go here </p>
                 </div>
-                <button className="Track-action"><!-- + or - will go here --></button>
+                {this.renderAction()}
             </div>
         )
     }
