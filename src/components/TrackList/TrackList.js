da6c61bc07599f998c5e2ee6defe392a3777bfbc
Daniel Echeverri <echeverri121@gmail.com>
2022-08-24 13:13:39 -0500

    step 33 completed

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -4,7 +4,7 @@ export class TrackList extends React.Component {
     render() {
         return(
             <div className="TrackList">
-            {/* You will add a map method that renders a set of Track components */}
+            {}
             </div>
         )
     }
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,23 +1,32 @@
 import './App.css';
+import React from "react";
 import {SearchBar} from '../SearchBar/SearchBar';
 import {SearchResults} from '../SearchResults/SearchResults';
 import {Playlist} from '../Playlist/Playlist';
 
 
-function App() {
-  return (
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {searchResults: [{name1: 'name1', artist1: 'artist1', album1: 'album1', id: 1}, 
+    {name2: 'name2', artist2: 'artist2', album2: 'album2', id: 2}, 
+    {name3: 'name3', artist3: 'artist3', album3: 'album3', id: 3}]};
+  }
+  
+  render(){
+    return (
     <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-    <SearchBar />
-    <div className="App-playlist">
-    <SearchResults />
-    <Playlist/>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+          <SearchResults searchResults={this.state.searchResults} />
+          <Playlist/>
+          </div>
+        </div>
     </div>
-  </div>
-</div>
-   
-  );
+     );
+    }
 }
 
 export default App;
