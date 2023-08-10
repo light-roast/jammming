ef33c909c5c665b0e5db76967471bea0b740bfb6
Daniel Echeverri <echeverri121@gmail.com>
2023-08-10 17:51:16 -0500

    Correct all components so that the app renders

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,8 @@
+export default function SearchBar() {
+    return (
+        <div className="SearchBar">
+            <input placeholder="Enter A Song, Album, or Artist" />
+            <button className="SearchButton">SEARCH</button>
+        </div>
+    )
+}
