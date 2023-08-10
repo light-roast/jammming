068c2071e16334abb0fb70b60bf4652e15e6fc72
Daniel Echeverri <echeverri121@gmail.com>
2023-08-10 17:06:41 -0500

    App xlm to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,16 @@ function App() {
 
   return (
     <>
-      
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+          <div class="App">
+            <!-- Add a SearchBar component -->
+            <div className="App-playlist">
+              <!-- Add a SearchResults component -->
+              <!-- Add a Playlist component -->
+            </div>
+          </div>
+      </div>
     </>
   )
 }
