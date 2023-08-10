9cd116881da9ffbab00e3194dcc21ed7dead6eda
Daniel Echeverri <echeverri121@gmail.com>
2023-08-10 18:05:45 -0500

    Inser XML to the component and import TrackList component

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,6 +1,10 @@
+import TrackList from '../TrackList/TrackList.jsx'
+
 export default function SearchResults() {
     return (
-        <>
-        </>
+        <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList/>
+      </div>
     )
 }
