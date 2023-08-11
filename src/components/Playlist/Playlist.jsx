df6cb94c63d2b619e51dd67338a471cfb758ca49
Daniel Echeverri <echeverri121@gmail.com>
2023-08-10 20:22:27 -0500

    Add xml and css to Playlist comp

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -1,6 +1,11 @@
+import TrackList from '../TrackList/TrackList.jsx'
+
 export default function Playlist() {
     return(
-        <>
-        </>
+        <div className="Playlist">
+            <input defaultValue={'New Playlist'}/>
+            <TrackList/>
+            <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        </div>
     )
 }
