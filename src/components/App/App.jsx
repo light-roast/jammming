d0131476b3c01fdaf75f886308e50d76da3391ca
Daniel Echeverri <echeverri121@gmail.com>
2023-08-15 10:38:32 -0500

    Add initial logic to handle search terms

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -72,12 +72,18 @@ function App() {
     const tracksUris = playlistTracks.map(track => track.uri);
   }
 
+  function search(term) {
+    console.log(term);
+  }
+
   return (
     <>
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
           <div className="App">
-            <SearchBar/>
+            <SearchBar
+              onSearch={search}
+            />
             <div className="App-playlist">
               <SearchResults searchResults={searchResults} 
                               onAdd={addTrack}
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import './SearchBar.css'
+import PropTypes from 'prop-types'
 
-export default function SearchBar() {
+
+export default function SearchBar({onSearch}) {
+    const [term, setTerm] = useState('');
+    function search() {
+        onSearch(term);
+    }
+    function handleTermSearch(e) {
+        setTerm(e.event.target)
+    }
     return (
         <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" />
+            <input onChange={handleTermSearch} placeholder="Enter A Song, Album, or Artist" />
             <button className="SearchButton">SEARCH</button>
         </div>
     )
 }
+
+SearchBar.propTypes = {
+    onSearch: PropTypes.func.isRequired
+    
+};
